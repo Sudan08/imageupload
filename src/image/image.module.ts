5c9a11ed35db7b03660960fac49c88a06825dc90
SUDAN_LAPTOP\sudan <sudanshakya08@gmail.com>
2023-02-20 18:59:33 +0545

    added images

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
-import { join } from 'path';
 
 @Module({
   controllers: [ImageController],
