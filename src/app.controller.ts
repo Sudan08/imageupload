5b00ef9741111cc43301c3c30ec315ac533c78b0
SUDAN_LAPTOP\sudan <sudanshakya08@gmail.com>
2023-02-22 21:43:42 +0545

    completed endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  // @Get()
+  // getImage(@Query('imageName') imageName : string, @Res() res: Response) {
+  //   // reading the file from the assests folder and sending the file
+  //   const file = 
+  // }
   @Get()
   getHello(): string {
     return 'Welcome to NestJS!';
--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get, Post, StreamableFile } from '@nestjs/common';
-import { UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
+import {
+  Controller,
+  Get,
+  Post,
+  StreamableFile,
+  Query,
+  Res,
+} from '@nestjs/common';
+import {
+  Param,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { join, extname } from 'path';
 import { BASEURL } from 'src/helper/helper';
+import { Observable, of } from 'rxjs';
 
 const storage = diskStorage({
   destination: './assests',
@@ -24,6 +36,9 @@ export class ImageController {
       return 'Error uploading file';
     }
   }
-}
 
-// @Post('upload')
+  @Get(':imageName')
+  getImage(@Param('imageName') imageName: string, @Res() res): Observable<any> {
+    return of(res.sendFile(join(process.cwd(), `assests/${imageName}`)));
+  }
+}
